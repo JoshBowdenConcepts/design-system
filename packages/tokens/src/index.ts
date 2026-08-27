4d1c4a6ef44c7c7a025c1b0203452868250f0eb9
Josh Bowden <joshbowden@Joshs-MBP.localdomain>
2026-08-27 16:30:24 -0400

    Added SpecKit

--- packages/tokens/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./types";
-export * from "./manifest";
-// The color palette is internal (referenced by semantic tokens) and is
-// intentionally not exported. Consume colors via `semanticColor`.
-export { semanticColor, space, fontFamily } from "./tokens";
